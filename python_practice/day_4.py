3250adb1a5bb0e55a0dcf17b4d871789dcee1654
Nikhil Kumar <49320663+nikhilniksss@users.noreply.github.com>
2025-03-11 11:49:56 +0530

    Add files via upload

---
+++ python_practice/day_4.py
@@ -0,0 +1,33 @@
+# numbers continues
+
+ipl = ['MI','CSK','LSG']
+
+ipl
+
+ipl.extend([1,2])
+
+ipl[1][0:2]
+
+a = []
+a.append(1)
+a
+
+len(ipl)
+len(a)
+
+ipl_string = "CSK,MI,GG"
+ipl_lst = ipl_string.split(",")
+ipl_lst
+
+
+ipl_string_1 = "CSK MI GG RCB LSG"
+ipl_lst_1 = ipl_string_1.split(" ")
+ipl_lst_1
+
+# pop method
+
+ipl_lst_1.pop() # last element removed
+ipl_lst_1.pop(0)
+ipl_lst_1
+
+
