70897b8524bdcd7d69d9de42e4b596efc400caa4
Nikhil Kumar <49320663+nikhilniksss@users.noreply.github.com>
2025-03-25 02:41:45 +0530

    Add files via upload

---
+++ python_practice/day_9.py
@@ -0,0 +1,23 @@
+# working with functions
+
+def greet():
+    print("Hello")
+
+greet()
+
+# sum of the numbers
+
+def sum_of_two_numbers(a,b):
+    return a + b
+
+sum_of_two_numbers(10,20)
+
+# function with default value
+
+def sum_of_two_numbers(a,b=1):
+    return a + b
+
+sum_of_two_numbers(10)
+sum_of_two_numbers(10,22)
+
+
